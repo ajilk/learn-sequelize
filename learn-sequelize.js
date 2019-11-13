d48ffc6713954939bde66ef3f98edddc02111c60
ajilk <ajilkhomov@gmail.com>
2019-11-12 20:37:14 -0500

    Update

--- learn-sequelize.js
+++ learn-sequelize.js
@@ -1,11 +1,11 @@
-const { Genre } = require('./models');
+const { Genre, Movie, Actor } = require('./models');
 
 /*
   currently, the genre table only has: Action, Adventure, and Drama
   Add one more Genre.
 */
-function insertNewGenre() {
-
+async function insertNewGenre() {
+  Genre.create({ name: 'Romance' });
 }
 
 /*
@@ -13,51 +13,59 @@ function insertNewGenre() {
   Add one more Movie of your choice. But it CANNOT be from 2008.
 */
 function insertNewMovie() {
-
+  Movie.create({ title: 'myMovie', year: '2019' })
 }
 
 /*
   Return the title of the movie with ID=2
 */
 function getMovieWithId2() {
-
+  return Movie.findByPk(2).get('title');
 }
 
 /*
   Return an array of all the actor names
 */
-function getAllActors() {
+async function getAllActors() {
+  const actors = await Actor.findAll()
+  const actorNames = actors.map(actor => actor.get('name'));
+  return actorNames
 
 }
 
 /*
   Return an array of all the movie names from 2008
 */
-function getAllMoviesFrom2008() {
-
+async function getAllMoviesFrom2008() {
+  const movies = await Movie.findAll({ where: { year: 2008 } });
+  return movies.map(movie => movie.get('title'));
 }
 
 /*
   Delete the genre you added in the first test
 */
 function deleteGenreYouAdded() {
-
+  Genre.destroy({ where: { name: 'Romance' } })
 }
 
 /*
   Rosario Dawson acted in the movie Eagle Eye.
   Add this association.
 */
-function associateRosarioToEagleEye() {
-
+async function associateRosarioToEagleEye() {
+  const actor = await Actor.findOne({ where: { name: 'Rosario Dawson' } })
+  const movie = await Movie.findOne({ where: { title: 'Eagle Eye' } })
+  actor.addMovie(movie);
 }
 
 /*
   Robert Downey Jr. acted in the movie Tropic Thunder.
   Add this association.
 */
-function associateRobertToTropicThunder() {
-
+async function associateRobertToTropicThunder() {
+  const actor = await Actor.findOne({ where: { name: 'Robert Downey Jr.' } })
+  const movie = await Movie.findOne({ where: { title: 'Tropic Thunder' } })
+  actor.addMovie(movie);
 }
 
 module.exports = {
